3b6459e0770fc14797a82d749a68c3c26f0dde55
bookwormpi <bookwormpi2.718@gmail.com>
2025-04-27 09:00:59 -0600

    Initial Commit

---
+++ src/client/java/org/bookwormpi/clientsidetesting/client/ClientsidetestingClient.java
@@ -0,0 +1,10 @@
+package org.bookwormpi.clientsidetesting.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientsidetestingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/bookwormpi/clientsidetesting/Clientsidetesting.java
@@ -0,0 +1,10 @@
+package org.bookwormpi.clientsidetesting;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Clientsidetesting implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
