33397f0c4330989687539d9621d0fc0cd04bad5f
bookwormpi <bookwormpi2.718@gmail.com>
2025-06-11 06:41:07 -0600

    testing build flow

--- src/client/java/org/bookwormpi/clientsidetesting/client/ui/BlockSearchConfigScreen.java
+++ src/client/java/org/bookwormpi/clientsidetesting/client/ui/BlockSearchConfigScreen.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class BlockSearchConfigScreen extends Screen {
     private final Screen parent;
     private final List<Block> allBlocks;
